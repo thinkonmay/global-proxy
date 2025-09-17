48306cc6eaf7fb1366bb88ba50ea0c926d42bc1b
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-09-17 13:11:33 +0700

    update

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"log"
@@ -13,15 +13,26 @@ import (
 	"os"
 	"os/signal"
 	"runtime/debug"
+	"strconv"
 	"strings"
 	"sync"
 	"syscall"
 	"time"
+
+	"golang.org/x/crypto/acme"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
-	// certdoms = []string{"play.2.thinkmay.net"}
-	sport = 3060
+	certdoms = []string{}
+	sport    = 0
+	rport    = 0
+	qport    = 0
+	cache    = analyticCache{
+		nodeMap: make(map[string]*nodeCache),
+		mut:     &sync.Mutex{},
+	}
+	credentialAnalytics = ""
 )
 
 type analytic struct {
@@ -39,122 +50,67 @@ type analyticCache struct {
 }
 
 func StartPocketbase() {
-	// Reverse proxies
-	// apiProxy := newReverseProxy("http://localhost:3001")
-	// rootProxy := newReverseProxy("http://localhost:3002")
+	mux := http.NewServeMux()
+	if len(certdoms) > 0 {
+		apiProxy := newReverseProxy("http://localhost:3001")
+		rootProxy := newReverseProxy("http://localhost:3002")
+		mux.Handle("/api/", apiProxy)
+		mux.Handle("/", rootProxy)
+		// Reverse proxies
+		rportAnalytics := newReverseProxy("http://localhost:3050")
+		// // HTTPS mux
+		mux.Handle("/report", rportAnalytics)
 
-	cache := analyticCache{
-		nodeMap: map[string]*nodeCache{},
-		mut:     &sync.Mutex{},
+		certManager := &autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			Cache:      autocert.DirCache("./pb_data/.autocert_cache"),
+			HostPolicy: autocert.HostWhitelist(certdoms...),
+		}
+		// base request context used for cancelling long running requests
+		// like the SSE connections
+		baseCtx, cancelBaseCtx := context.WithCancel(context.Background())
+		defer cancelBaseCtx()
+
+		server := &http.Server{
+			BaseContext: func(l net.Listener) context.Context { return baseCtx },
+			TLSConfig: &tls.Config{
+				MinVersion:     tls.VersionTLS12,
+				GetCertificate: certManager.GetCertificate,
+				NextProtos:     []string{acme.ALPNProto},
+			},
+
+			ReadTimeout:       10 * time.Minute,
+			ReadHeaderTimeout: 30 * time.Second,
+			Addr:              fmt.Sprintf(":%d", sport),
+			Handler:           mux,
+		}
+		panic(server.ListenAndServe())
 	}
-	// HTTPS mux
-	// mux := http.NewServeMux()
-	// mux.Handle("/api/", apiProxy)
-	// mux.Handle("/", rootProxy)
-
-	// certManager := &autocert.Manager{
-	// 	Prompt:     autocert.AcceptTOS,
-	// 	Cache:      autocert.DirCache("./pb_data/.autocert_cache"),
-	// 	HostPolicy: autocert.HostWhitelist(certdoms...),
-	// }
-	// base request context used for cancelling long running requests
-	// like the SSE connections
-	baseCtx, cancelBaseCtx := context.WithCancel(context.Background())
-	defer cancelBaseCtx()
-
-	publicMux := http.NewServeMux()
-	publicMux.HandleFunc("/report", func(w http.ResponseWriter, r *http.Request) {
-		node := r.Header.Get("node")
-		t := r.Header.Get("type")
-		if credential := r.Header.Get("Authorization"); credential != "abc" {
-			http.Error(w, "unauthorized", http.StatusUnauthorized)
-			return
-		}
-		data, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "bad request", http.StatusBadRequest)
-			return
-		}
-
-		cache.mut.Lock()
-		defer cache.mut.Unlock()
-		if nodeData, found := cache.nodeMap[node]; found {
-			nodeData.mut.Lock()
-			nodeData.typeMap[t] = &analytic{timestamp: time.Now(), data: data}
-			nodeData.mut.Unlock()
-		} else {
-			cache.nodeMap[node] = &nodeCache{
-				mut: &sync.Mutex{},
-				typeMap: map[string]*analytic{
-					t: {timestamp: time.Now(), data: data},
-				},
-			}
-		}
-	})
-
-	privateMux := http.NewServeMux()
-	privateMux.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
-		if credential := r.Header.Get("Authorization"); credential != "abc" {
-			http.Error(w, "unauthorized", http.StatusUnauthorized)
-			return
-		}
-
-		destnode := r.Header.Get("node")
-		desttyp := r.Header.Get("type")
-
-		cache.mut.Lock()
-		defer cache.mut.Unlock()
-
-		nodeData, found := cache.nodeMap[destnode]
-		if !found {
-			return
-		}
-
-		nodeData.mut.Lock()
-		defer nodeData.mut.Unlock()
-
-		dataJSON, _ := json.Marshal(nodeData.typeMap[desttyp])
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(dataJSON)
-		return
-	})
-
-	server := &http.Server{
-		BaseContext: func(l net.Listener) context.Context { return baseCtx },
-		// TLSConfig: &tls.Config{
-		// 	MinVersion:     tls.VersionTLS12,
-		// 	GetCertificate: certManager.GetCertificate,
-		// 	NextProtos:     []string{acme.ALPNProto},
-		// },
-
-		ReadTimeout:       10 * time.Minute,
-		ReadHeaderTimeout: 30 * time.Second,
-		Addr:              fmt.Sprintf(":%d", sport),
-		Handler:           publicMux,
-	}
-
-	privateServer := &http.Server{
-		Addr:    ":3050",
-		Handler: privateMux,
-	}
-
-	go func() {
-		if err := privateServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("private server error: %v", err)
-		}
-	}()
-
-	panic(server.ListenAndServe())
 }
 
 func main() {
-	go StartPocketbase()
+	args := os.Args
+	var err error
+	if sport, err = strconv.Atoi(args[1]); err != nil {
+		return
+	} else if qport, err = strconv.Atoi(args[2]); err != nil {
+		return
+	} else if rport, err = strconv.Atoi(args[3]); err != nil {
+		return
+	} else if credentialAnalytics = args[4]; len(credentialAnalytics) == 0 {
+		return
+	} else {
+		certdoms = args[5:]
+		SafeThread(StartPocketbase)
+		SafeThread(StartQueryAnalytics)
+		SafeThread(StartReportAnalytics)
 
-	sigchan := make(chan os.Signal, 16)
-	signal.Notify(sigchan, syscall.SIGTERM, os.Interrupt)
-	sig := <-sigchan
+		sigchan := make(chan os.Signal, 16)
+		signal.Notify(sigchan, syscall.SIGTERM, os.Interrupt)
+		sig := <-sigchan
 
-	fmt.Printf("receive signal %s from os\n", sig.String())
+		fmt.Printf("receive signal %s from os\n", sig.String())
+	}
 }
 
 func SafeLoop(sleep_period time.Duration, fun func() bool) {
@@ -226,6 +182,73 @@ func newReverseProxy(target string) *httputil.ReverseProxy {
 	}
 
 	return proxy
+}
+
+func StartReportAnalytics() {
+	publicMux := http.NewServeMux()
+	publicMux.HandleFunc("/report", func(w http.ResponseWriter, r *http.Request) {
+		if credential := r.Header.Get("Authorization"); credential != credentialAnalytics {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		} else if typ := r.Header.Get("type"); len(typ) == 0 {
+			return
+		} else if node := r.Header.Get("node"); len(node) == 0 {
+			return
+		} else if data, err := io.ReadAll(r.Body); err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		} else {
+			cache.mut.Lock()
+			defer cache.mut.Unlock()
+			if nodeData, found := cache.nodeMap[node]; found {
+				nodeData.mut.Lock()
+				nodeData.typeMap[typ] = &analytic{timestamp: time.Now(), data: data}
+				nodeData.mut.Unlock()
+			} else {
+				cache.nodeMap[node] = &nodeCache{
+					mut: &sync.Mutex{},
+					typeMap: map[string]*analytic{
+						typ: {timestamp: time.Now(), data: data},
+					},
+				}
+			}
+		}
+	})
+	publicSever := &http.Server{
+		Addr:    fmt.Sprintf(":%d", rport),
+		Handler: publicMux,
+	}
+	panic(publicSever.ListenAndServe())
+}
+
+func StartQueryAnalytics() {
+	privateMux := http.NewServeMux()
+	privateMux.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
+		if credential := r.Header.Get("Authorization"); credential != credentialAnalytics {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		} else if destnode := r.Header.Get("node"); len(destnode) == 0 {
+			return
+		} else if desttyp := r.Header.Get("type"); len(desttyp) == 0 {
+		} else {
+			cache.mut.Lock()
+			defer cache.mut.Unlock()
+			if nodeData, found := cache.nodeMap[destnode]; !found {
+				return
+			} else {
+				nodeData.mut.Lock()
+				defer nodeData.mut.Unlock()
+				w.Write(nodeData.typeMap[desttyp].data)
+				return
+			}
+		}
+	})
+
+	privateServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", qport),
+		Handler: privateMux,
+	}
+	panic(privateServer.ListenAndServe())
 }
 
 func SafeThread(fun func()) {
